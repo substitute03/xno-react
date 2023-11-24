fddb6ca197dc2d0d9392f37451ccfe6d156f2445
Tom Naylor <tnaylor@defaqto.com>
2023-11-24 13:43:03 +0000

    Add a Game component.

---
+++ components/game.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import GameBoard from './gameBoard';
+import { GameStateUpdate, State } from '../models/gameState';
+
+
+
+export default function Game() {
+   const[message, setMessage] = useState("");
+
+  // Callback function to receive winner information from GameBoard
+  const handleGameStateUpdate = (gameState: GameStateUpdate) => {
+   gameState.state === State.WinnerFound
+   ? setMessage(`${gameState.description} wins! 🎉`)
+   : setMessage(`${gameState.description}`)
+ };
+
+
+  return (
+   <>
+   <div className='row col-2 offset-5 fs-3 text-center mt-5'>
+      <img src="./XNO_Logo.png" alt="logo"></img>
+   </div>
+   <div className='row col-12 fs-5 text-center mt-4'>
+      <p>{message}</p>
+   </div>
+   <GameBoard gameStateUpdate={handleGameStateUpdate}></GameBoard>
+   </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,12 @@
 import { useState } from 'react';
 import GameBoard from '../../components/gameBoard'
 import { GameStateUpdate, State } from '../../models/gameState';
+import Game from '../../components/game';
 
 export default function Home() {
-   const[message, setMessage] = useState("");
-
-  // Callback function to receive winner information from GameBoard
-  const handleGameStateUpdate = (gameState: GameStateUpdate) => {
-   gameState.state === State.WinnerFound
-   ? setMessage(`${gameState.description} wins! 🎉`)
-   : setMessage(`${gameState.description}`)
- };
-
-
   return (
    <>
-   <div className='row col-2 offset-5 fs-3 text-center mt-5'>
-      <img src="./XNO_Logo.png" alt="logo"></img>
-   </div>
-   <div className='row col-12 fs-5 text-center mt-4'>
-      <p>{message}</p>
-   </div>
-   <GameBoard gameStateUpdate={handleGameStateUpdate}></GameBoard>
+   <Game></Game>
    </>
   )
 }
